ff9bc1fd323434d5faf459bf10aada328b405a4b
Code Sandbox <hello@codesandbox.io>
2022-02-15 02:21:27 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.ff065103db2d478f095542179441fef5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "db5b8a7d3bb5f5483187",
+    "url": "https://elaine-codes.github.io/csb-8jbqc/static/css/main.8cab7685.chunk.css"
+  },
+  {
+    "revision": "db5b8a7d3bb5f5483187",
+    "url": "https://elaine-codes.github.io/csb-8jbqc/static/js/main.6c422d58.chunk.js"
+  },
+  {
+    "revision": "b3f5c1a1d17eabf345d6",
+    "url": "https://elaine-codes.github.io/csb-8jbqc/static/js/runtime~main.005ada35.js"
+  },
+  {
+    "revision": "4f1e19e6e07ead45ea90",
+    "url": "https://elaine-codes.github.io/csb-8jbqc/static/js/2.f3d7e44e.chunk.js"
+  },
+  {
+    "revision": "df3e188a150fb289e9c2c1c7a54f3985",
+    "url": "https://elaine-codes.github.io/csb-8jbqc/index.html"
+  }
+];
